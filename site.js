4c696c6db178299649b5ba9a790fd50104c8e7b3
JakeCherichello <Cherichello1104@icloud.com>
2024-12-16 10:03:26 -0500

    kinda works did some wrong here tho

--- site.js
+++ site.js
@@ -5,41 +5,38 @@
 /* NOTE: MOVIES.JSON CONTAINS A LIST OF MOVIES AND ACCOMPANYING METADATA
 //
 //    They are in the following format:
-//    title (String): the name of the movie
-//    iscore (Number): the IMDB score
+//    title (String): the name of the movie */
+/*    iscore (Number): the IMDB score
 //    rating (String): the movie's MPAA rating
 //    released (Array): the release date. Note that the order of the array is:  YYYY, MM, DD
 //    country (String): the country of production
-//    posters (Array): an array of String values with the URL to movie posters (in your img/ directory)
-//    imdb (String): the URL to the corresponding IMDB website
-//    website (String): the URL to the corresponding official website
-//    likes (Number): a fictitious number of user likes
-//    dislikes (Number): a fictitious number of user dislikes
-//    posterindex (Number): a counter to use with the "posters" array to keep track of the current displayed poster index
-//
+@@ -20,26 +21,28 @@
 // FOR STEP 16, ADD THREE OF YOUR OWN FAVORITE MOVIES WITH METADATA TO THE END OF THE JSON FILE LIST
 */
-
 
 const vue_app = Vue.createApp({
       // This automatically imports your movies.json file and puts it into
       //   the variable: movies
-      created () {
-            fetch('movies.json').then(response => response.json()).then(json => {
-                  this.movies = json
-            })
+      created() {
+        fetch("movies.json")
+          .then((response) => response.json())
+          .then((json) => {
+            this.movies = json;
+          });
       },
       data() {
-        return {
-            // This holds your movies.json data.
-            movies: [],
-            /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
-         
-      }
-    },
+        return {     
+           movies: [],
+              /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
+          title: "IMDB + YourName's Top 8 Movies",
+          owner: "Ben",
+          github: "https://github.com/Gkilgore/NJIT-3_StarterFiles",
+        };
+      },
       methods: {
-            /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
-      }
-})
+        /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
+      },
+    });
+    
+    vue_app.mount("#vue_app");
 
-vue_app.mount("#vue_app")

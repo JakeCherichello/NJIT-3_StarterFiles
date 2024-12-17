3cb7db15f4ccb389d6f62b0943026294e01ba942
JakeCherichello <Cherichello1104@icloud.com>
2024-12-17 11:23:55 -0500

    almost done need to style and fix some bugs

--- site.js
+++ site.js
@@ -5,9 +5,8 @@
 /* NOTE: MOVIES.JSON CONTAINS A LIST OF MOVIES AND ACCOMPANYING METADATA
 //
 //    They are in the following format:
-//    title (String): the name of the movie */
-
-/*    iscore (Number): the IMDB score
+//    title (String): the name of the movie
+//    iscore (Number): the IMDB score
 //    rating (String): the movie's MPAA rating
 //    released (Array): the release date. Note that the order of the array is:  YYYY, MM, DD
 //    country (String): the country of production
@@ -21,89 +20,48 @@
 // FOR STEP 16, ADD THREE OF YOUR OWN FAVORITE MOVIES WITH METADATA TO THE END OF THE JSON FILE LIST
 */
 
+
 const vue_app = Vue.createApp({
   // This automatically imports your movies.json file and puts it into
   //   the variable: movies
-  created() {
-    fetch("movies.json")
-      .then((response) => response.json())
-      .then((json) => {
-        this.movies = json;
-      });
+  created () {
+        fetch('movies.json').then(response => response.json()).then(json => {
+              this.movies = json
+        })
   },
   data() {
     return {
-      movies: [],
-      /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
-      title: "IMDB + YourName's Top 8 Movies",
-      owner: "Jake Cherichello",
-      github: "https://github.com/Gkilgore/NJIT-3_StarterFiles",
-    };
-  },
-  methods: {
-    /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
-    getMonthText: function (dateArray) {
-      const [year, month, day] = dateArray;
-      let monthText = " ";
-      let suffix = " ";
-      switch (month) {
-        case 1:
-          monthText = "January";
-          break;
-        case 2:
-          monthText = "February";
-          break;
-        case 3:
-          monthText = "March";
-          break;
-        case 4:
-          monthText = "April";
-          break;
-        case 5:
-          monthText = "May";
-          break;
-        case 6:
-          monthText = "June";
-          break;
-        case 7:
-          monthText = "July";
-          break;
-        case 8:
-          monthText = "August";
-          break;
-        case 9:
-          monthText = "September";
-          break;
-        case 10:
-          monthText = "October";
-          break;
-        case 11:
-          monthText = "November";
-          break;
-          case 12:
-          monthText = "December";
-          break;
-        default:
-          monthText = "Invalid";
-      }
-      if (day == 1 || day == 21 || day == 31) {
-        suffix = "st";
-      } else if (day == 2 || day == 22) {
-        suffix = "nd";
-      } else if (day == 3 || day == 23) {
-        suffix = "rd";
-      } else {
-        suffix = "th";
-      }
-      return `${monthText} ${day}${suffix}, ${year}`;
-    },
-    posterClick: function() {
-      if (movies.posterindex > movies.posters.length - 1)
-           movies.posterindex = 0;
-      else 
-           movies.posterindex++;
-  },
+        // This holds your movies.json data.
+        movies: [],
+        /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
+        title: 'IMDB + Jake Cherichellos Top 8 Movies',
+        owner: 'Jake Cherichello',
+        github: 'https://github.com/jcherichello/NJIT-Project-2'
+  }
 },
-});
+  methods: {
+        /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
+        getMonthText(dateArray) {
+              const months = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+              const [year, month, day] = dateArray;
+              return `${months[month - 1]} ${day}, ${year}`;
+        },
+        like(index){
+              this.movies[index].likes++;
+        },
+        dislike(index){
+              this.movies[index].dislikes++;
+        },
+        posterClick(index){
+              const movie = this.movies[index];
+              movie.posterindex = (movie.posterindex + 1) % movie.posters.length;            
+        },
+        timeText(minutes){
+              const hours = Math.trunc(minutes / 60)
+              const mins = minutes % 60
+              return `${hours}h ${mins}m`
+        },
+  }
+})
 
-vue_app.mount("#vue_app");
+vue_app.mount("#vue_app")
